212cba52f186b1333f142d00e9089db81bddc0f5
Sashe-Bashe <alexandermoev1@gmail.com>
2022-12-12 18:00:00 -0500

    Second notes done

---
+++ Notes For Begginer Lessons/2.py
@@ -0,0 +1,40 @@
+# Tag Eins wieder lernen
+# Ден Първи учене отново
+# Day One Relearn
+
+# A string is a sequence of characters
+string = "This is a string"
+
+# int is a integer which is whole numbers
+num = int(2)
+
+# a float is a decimal
+num2 = float(5.6)
+
+# Allows for compex numbers for example 4 + 5i
+num3 = complex(4, 5)
+
+# A true or false value (Richtig order Falsch)
+check = False
+
+# Allows for the storage of values
+myList = ["Chicken", "Horse", "Lizard"]
+
+# Tuple allows for storage of values but can't change them
+myTuple = ("strawberry", "Chocolate", "cherry")
+
+# Dictionaries are used to store data values in key:value pairs.
+myDict = {
+    "brand": "Ford",
+    "model": "F150",
+    "year": 2020
+}
+
+# Output the result for which type of datatype it is
+print(type(10))
+
+# In Python you can always change the Datatype
+x = 10
+x = True
+
+
