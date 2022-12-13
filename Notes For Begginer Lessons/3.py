5c81b66fc462a8818c4dbec471fdb05f4afc9c34
Sashe-Bashe <alexandermoev1@gmail.com>
2022-12-13 16:26:42 -0500

    The third lesson

---
+++ Notes For Begginer Lessons/3.py
@@ -0,0 +1,28 @@
+# Modulus prints the remainder
+print(12 % 5)
+# Exponent in Python
+print(4 ** 5)
+# Floor Division gives the rounded quotient
+print(17 // 2)
+# Assignment operator does an operation and makes it equal to that var.
+# For example with addition operator:
+x = 10
+x += 5
+print(x)  # x will be 15
+
+# Comparison operators
+print(1 == 1)  # equal to
+print(1 != 2)  # not equal to
+print(2 > 1)  # greater than
+print(1 < 2)  # less than
+print(2 >= 2)  # greater than or equal to
+print(3 <= 3)  # less than or equal to
+
+# bitwise operators that return a boolean
+print(30 > 10 and 20 == 20)  # returns true when both comparisons are true
+print(20 > 10 or 20)  # returns true when at least one comparison is true
+print(not True)  # this will return false
+print(not 2 > 1)  # this will false
+
+# taking in input from user in console
+x = input("Enter Number a letter: ")  # this will always return a string
